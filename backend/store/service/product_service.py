35f2cab0d35f568ed0cbf49d2d04d8c59634e440
Won <jungwon1423@gamil.com>
2021-05-02 21:58:42 +0900

    [store > 상품상세] - 상품상세 정보 가져오기 - 상품 색상에 따른 사이즈 정보 가져오기 - 같은 셀러 다른 상품 가져오기 - 상품 QNA 가져오기

--- backend/store/service/product_service.py
+++ backend/store/service/product_service.py
@@ -1,9 +1,21 @@
+from flask import g
 from model import ProductDao
 from datetime import timedelta, datetime
-from utils.custom_exception import StartDateFail
+from utils.custom_exception import StartDateFail, DataNotExists
 import xlwt
 from io import BytesIO
+import copy
 # from flask import send_file
+
+# discount date를 확인하고 discount_rate, discount_price 반환
+def discount(discount_rate, start_date, end_date, price):
+        if start_date <= datetime.now() and end_date >= datetime.now():
+            discount_price = price - discount_rate * price
+            return discount_rate, discount_price
+        else:
+            discount_rate = 0
+            discount_price = price - discount_rate * price
+            return discount_rate, discount_price
 
 class ProductService:
     def __new__(cls, *args, **kwargs):
@@ -13,4 +25,145 @@ class ProductService:
 
     def __init__(self):
         self.product_dao = ProductDao()
-  
+    
+    def get_product_detail(self, conn, params):
+        # 데이터베이스에 상품 존재하는지 확인
+        exists = self.product_dao.check_product_exists(conn, params)
+        if not exists['count']:
+            raise DataNotExists('상품이 존재하지 않습니다.', 'product does not exist')
+
+        # 상품 상세, 상품이미지, 상품옵션, 셀러의 다른상품 가져오기
+        product_detail, product_image, product_option, other_product = self.product_dao.get_product_detail(conn, params)
+        
+        # 상품의 discount_rate, discount_price 입력
+        product_detail['discount_rate'], product_detail['discount_price'] = discount(product_detail['discount_rate'], product_detail['discount_start_date'], product_detail['discount_end_date'], product_detail['price'])
+
+        # 셀러의 다른상품 discount_rate, discount_price 입력
+        for product in other_product:
+            product['discount_rate'], product['discount_price'] = discount(product['discount_rate'], product['discount_start_date'], product['discount_end_date'], product['price'])
+
+        result = {
+            # 상품 아이디
+            'product_id' : product_detail['product_id'],
+            # 셀러 아이디
+            "seller_id" : product_detail['seller_id'],
+            # 셀러 한국 이름
+            "seller_korean_name" : product_detail['seller_korean_name'],
+            # 상품 제목
+            "product_title" : product_detail['title'],
+            # 상품 가격
+            "price" : product_detail['price'],
+            # 상품 할인율
+            "discount_rate" : product_detail['discount_rate'],
+            # 상품 할인가격
+            "dicount_price" : product_detail['discount_price'],
+            # 상품 상세정보
+            "detail_description" : product_detail['detail_description'],
+            # 상품의 이미지, 대표이미지 여부
+            "images" : [
+                {
+                    "image_url" : image['image_url'],
+                    "is_represent" : image['is_represent']
+                }
+            for image in product_image],
+            # 상품의 옵션 중 전체 색상
+            "color_size" : [
+                {
+                    'id' : option['color_id'],
+                    'name' : option['name']
+                }
+            for option in product_option],
+            # 셀러의 다른 상품 최신순 5개
+            'other_product' : [
+                {
+                    "image_url" : other['image_url'],
+                    "title" : other['title'],
+                    "price" : other['price'],
+                    "discount_rate" : other['discount_rate'],
+                    "discount_price" : other['discount_price'],
+                    "product_id" : other['product_id']
+                }
+            for other in other_product]
+        }
+
+        return result
+    
+    def get_product_option(self, conn, params):
+        # 데이터베이스에 상품 존재하는지 확인
+        exists = self.product_dao.check_product_exists(conn, params)
+        if not exists['count']:
+            raise DataNotExists('상품이 존재하지 않습니다.', 'product does not exist')
+        
+        # 데이터베이스에 상품 색상이 존재하는지 확인
+        exists = self.product_dao.check_product_color_exists(conn, params)
+        if not exists['count']:
+            raise DataNotExists('상품 색상이 존재하지 않습니다.', 'product color does not exist')
+        
+        return self.product_dao.get_product_option(conn, params)
+
+    def get_product_question_answer(self, conn, params):
+        # 요청으로 들어온 값을 복합객체, 내용 복사
+        request_data = copy.deepcopy(params)
+        
+        # 페이지네이션 위한 OffSET 설정
+        request_data['offset'] = (request_data['page'] - 1) * request_data['limit']
+
+        # 데이터베이스에 상품 존재하는지 확인
+        exists = self.product_dao.check_product_exists(conn, params)
+        if not exists['count']:
+            raise DataNotExists('상품이 존재하지 않습니다.', 'product does not exist')
+
+        # 데이터베이스에 질문이 없다면 return
+        total_count_question = self.product_dao.get_total_count_question(conn, request_data)
+        if total_count_question == 0:
+            return total_count_question
+
+        # 질문 정보, 답변 정보
+        question_result, answer_result = self.product_dao.get_product_question_answer(conn, request_data)
+
+        result = {
+            # 질문 총 갯수
+            'total_count' : total_count_question['total_count'],
+            # 질문 정보
+            'questions' : [
+                {   
+                    # 질문 아이디
+                    "question_id" : question['question_id'],
+                    # 질문 타입 정보
+                    "question_type_name" : question['question_type_name'],
+                    # 질문 타입 아이디
+                    "question_type_id" : question['question_type_id'],
+                    # 질문 답변여부
+                    "question_answer_status" : question['question_answer_status'],
+                    # 질문 내용
+                    "question_content" : question['question_content'],
+                    # 질문 등록 유저 identification
+                    "user_identification" : question['question_user_identification'],
+                    # 질문 등록 유저 아이디
+                    "user_id" : question['question_user_id'],
+                    # 질문 등록일
+                    "question_upload_date" : question['question_upload_date'],
+                    # 질문 공개 여부
+                    "question_secret_status" : question['question_secret_status'] if question['account_id'] != g.account_id else 0,
+                    # 답변 정보
+                    'answer' : [
+                        {   
+                            # 답변 아이디
+                            "answer_id" : answer['answer_id'],
+                            # 답변한 질문 아이디
+                            "answer_question_id" : answer['answer_question_id'],
+                            # 답변 내용
+                            "answer_content" : answer['answer_content'],
+                            # 답변 등록일
+                            "answer_upload_date" : answer['answer_upload_date'],
+                            # 답변 유저 identification
+                            "answer_identification" : answer['name'],
+                            # 답변 유저 아이디
+                            "answer_account_id" : answer['account_id']
+                        }
+                    for answer in answer_result if answer['answer_question_id'] == question['question_id']]
+                }
+            for question in question_result]
+        }
+
+        return result
--- backend/store/view/product_view.py
+++ backend/store/view/product_view.py
@@ -1,9 +1,10 @@
 from flask import request, jsonify, g, send_file
 from flask.views import MethodView
-from flask_request_validator import validate_params, Param, GET, Datetime, ValidRequest, CompositeRule, Min, Max, Enum, JsonParam, JSON, HEADER
+from flask_request_validator import validate_params, PATH, Param, GET, Datetime, ValidRequest, CompositeRule, Min, Max, Enum, JsonParam, JSON, HEADER
 from datetime import datetime
 from connection import get_connection
 from utils.response import get_response, post_response
+from utils.decorator import LoginRequired, LoginCheck
 from utils.custom_exception import IsInt, IsStr, IsFloat, IsRequired, DatabaseCloseFail
 from flask_request_validator.exceptions import InvalidRequestError, RulesError
 import xlwt
@@ -18,3 +19,70 @@ class ProductView(MethodView):
 class ProductDetailView(MethodView):
     def __init__(self, service):
         self.service = service
+
+    @validate_params(
+        Param('product_id', PATH, int)
+    )
+    def get(self, valid: ValidRequest, product_id):
+        conn = None
+        try:
+            conn = get_connection()
+            params = valid.get_path_params()
+            result = self.service.get_product_detail(conn, params)
+
+            return get_response(result)
+
+        finally:
+            try:
+                conn.close()
+            except Exception as e:
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
+
+class ProductOptionView(MethodView):
+    def __init__(self, service):
+        self.service = service
+    
+    @validate_params(
+        Param('product_id', PATH, int),
+        Param('color_id', PATH, int)
+    )
+    def get(self, valid:ValidRequest, product_id, color_id):
+        conn = None
+        try:
+            conn = get_connection()
+            params = valid.get_path_params()
+
+            result = self.service.get_product_option(conn, params)
+
+            return get_response(result)
+
+        finally:
+            try:
+                conn.close()
+            except Exception as e:
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
+
+class ProductQuestionAnswerView(MethodView):
+    def __init__(self, service):
+        self.service = service
+    
+    @validate_params(
+        Param('product_id', PATH, int),
+        Param('page', GET, int, required=False, default=1, rules=[Min(1)]),
+        Param('limit', GET, int, required=False, default=5, rules=CompositeRule(Min(5), Max(50)))
+    )
+    @LoginCheck('user')
+    def get(self, valid:ValidRequest, product_id):
+        conn = None
+        try:
+            conn = get_connection()
+            params = valid.get_params()
+            params['product_id'] = product_id
+
+            return get_response(self.service.get_product_question_answer(conn, params))
+
+        finally:
+            try:
+                conn.close()
+            except Exception as e:
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
