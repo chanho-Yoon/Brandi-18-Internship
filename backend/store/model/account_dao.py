ee2b4a3c49c75ef5f5ecc7ee760a69655b9810c9
chanho-Yoon <chyoon0512@gmail.com>
2021-05-02 15:06:08 +0900

    일반 로그인, 회원가입 기능 구현 소셜 로그인 기능 구현 ( 구글 ) 	- 일반 유저와 같이 account 생성 -> user 생성 -> user history 생성 -> social users 생성

--- backend/store/model/account_dao.py
+++ backend/store/model/account_dao.py
@@ -26,6 +26,25 @@ class AccountDao:
             cursor.execute(sql, params)
             return cursor.fetchone()
     
+    def get_account_type_id(self, conn, params):
+        """account_type_id를 가져오는 함수
+
+        Args:
+            conn (class): DB 클래스
+            params (dict): seller id값을 이용해서 가져온 seller_info 정보
+        """
+        sql = """
+            SELECT
+                account_type_id
+            FROM
+                account
+            WHERE
+                id = %(account_id)s
+        """
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params)
+            return cursor.fetchone()
+    
     def get_email(self, conn, params):
         """user email 중복 확인하는 함수
 
@@ -85,6 +104,29 @@ class AccountDao:
             cursor.execute(sql, params)
             return cursor.lastrowid
         
+    def create_social_user_signup(self, conn, params):
+        """social_user 생성하는 함수
+
+        Args:
+            conn (class): DB 클래스
+            params (dict): BODY에서 넘어온 social user 회원가입 정보
+        """
+        sql = """
+            INSERT INTO social_users (
+                user_id,
+                social_type_id,
+                social_identification
+            )
+            VALUES (
+                %(user_id)s,
+                %(social_type_id)s,
+                %(id)s
+            )
+        """
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params)
+            return cursor.lastrowid
+    
     def create_user_signup(self, conn, params):
         """user 생성하는 함수
 
@@ -92,52 +134,148 @@ class AccountDao:
             conn (class): DB 클래스
             params (dict): BODY에서 넘어온 user 회원가입 정보
         """
-        sql = """
-            INSERT INTO users (
-                account_id,
-                user_type_id,
-                user_identification,
-                email,
-                password,
-                phone,
-                receiving_event_is_agreed,
-                notifying_benefit_is_agreed
-            )
-            VALUES (
-                %(account_id)s,
-                %(user_type_id)s,
-                %(id)s,
-                %(email)s,
-                %(password)s,
-                %(phone)s,
-                %(receiving_event_is_agreed)s,
-                %(notifying_benefit_is_agreed)s
-            )
-        """
+        print(params)
+        if params['social'] == "google": 
+            sql = """
+                INSERT INTO users (
+                    account_id,
+                    user_type_id,
+                    user_identification,
+                    email,
+                    receiving_event_is_agreed,
+                    notifying_benefit_is_agreed   
+                )
+                VALUES (
+                    %(account_id)s,
+                    %(user_type_id)s,
+                    %(id)s,
+                    %(email)s,
+                    %(receiving_event_is_agreed)s,
+                    %(notifying_benefit_is_agreed)s
+                )
+            """
+        elif params['user'] == "user":
+            sql = """
+                INSERT INTO users (
+                    account_id,
+                    user_type_id,
+                    user_identification,
+                    email,
+                    password,
+                    phone,
+                    receiving_event_is_agreed,
+                    notifying_benefit_is_agreed
+                )
+                VALUES (
+                    %(account_id)s,
+                    %(user_type_id)s,
+                    %(id)s,
+                    %(email)s,
+                    %(password)s,
+                    %(phone)s,
+                    %(receiving_event_is_agreed)s,
+                    %(notifying_benefit_is_agreed)s
+                )
+            """
         with conn.cursor() as cursor:
             cursor.execute(sql, params)
             return cursor.lastrowid
     
-    def cerate_user_history(self, conn, params):
+    def create_user_history(self, conn, params):
         """user history 생성하는 함수
 
         Args:
             conn (class): DB 클래스
             params (dict): BODY에서 넘어온 user 회원가입 정보
         """
-        sql = """
-            INSERT INTO users_history (
-                user_id,
-                phone,
-                receiving_event_is_agreed,
-                notifying_benefit_is_agreed
-            ) VALUES (
-                %(user_id)s,
-                %(phone)s,
-                %(receiving_event_is_agreed)s,
-                %(notifying_benefit_is_agreed)s
-            )
-        """
+        if params['social'] == "google": 
+            sql = """
+                INSERT INTO users_history (
+                    user_id,
+                    receiving_event_is_agreed,
+                    notifying_benefit_is_agreed
+                ) VALUES (
+                    %(user_id)s,
+                    %(receiving_event_is_agreed)s,
+                    %(notifying_benefit_is_agreed)s
+                )
+            """
+        elif params['social'] == "user":
+            sql = """
+                INSERT INTO users_history (
+                    user_id,
+                    phone,
+                    receiving_event_is_agreed,
+                    notifying_benefit_is_agreed
+                ) VALUES (
+                    %(user_id)s,
+                    %(phone)s,
+                    %(receiving_event_is_agreed)s,
+                    %(notifying_benefit_is_agreed)s
+                )
+            """
         with conn.cursor() as cursor:
             cursor.execute(sql, params)
             return cursor.lastrowid
+        
+    def post_user_login(self, conn, params):
+        """user 로그인
+
+        Args:
+            conn (class): DB 클래스
+            params (dict): BODY에서 넘어온 user 정보
+        """
+        sql = """
+            SELECT
+                user_identification, password, is_deleted, account_id
+            FROM
+                users
+            WHERE
+                is_deleted = 0
+                AND user_identification = %(id)s
+        """
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params)
+            return cursor.fetchone()
+    
+    def post_social_login(self, conn, params):
+        """social user 로그인 
+
+        Args:
+            conn (class): DB 클래스
+            params ([type]): BODY에서 넘어온 social user 정보
+        """
+        sql = """
+            SELECT
+                user_identification, id_deleted, account_id
+            FROM
+                users
+            WHERE
+                is_deleted = 0,
+                AND user_identifacation = %(id)s
+        """
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params)
+            return cursor.fechone()
+     
+    def get_social_user_identification(self, conn, params):
+        """social users 중복 확인 하는 함수
+        
+        social 로그인을 하는데 가입된 아이디가 있는지 없는지 판단하는 함수
+
+        Args:
+            conn (class): DB 클래스
+            params (dict): 구글 로그인 요청할 때 받아온 user 정보
+        """
+        sql = """
+            SELECT
+                social_identification
+            FROM
+                social_users
+            WHERE
+                social_identification = %(id)s
+        """
+        with conn.cursor() as cursor:
+            cursor.execute(sql, params)
+            return cursor.fetchone()
+        pass
--- backend/store/view/account_view.py
+++ backend/store/view/account_view.py
@@ -46,8 +46,72 @@ class SignUpView(MethodView):
 class SignInView(MethodView):
     def __init__(self, service):
         self.service = service
+        
+    @validate_params(
+        Param('id', JSON, str, rules=[Pattern("^[a-z]+[a-z0-9@.]{4,19}$")], required=True),
+        Param('password', JSON, str, rules=[Pattern('^[A-Za-z0-9@#$]{6,12}$')], required=True),
+        Param('social', JSON, str, required=True)
+    )
+    def post(self, valid):
+        conn = None
+        try:
+            body = valid.get_json()        
+            conn = get_connection()
+
+            result = self.service.post_user_login(conn, body)
+            
+            return post_response({
+                        "message" : "success", 
+                        "accessToken" : result['accessToken'],
+                        "account_type_id" : result['account_type_id'],
+                        "status_code" : 200
+                        }), 200
+        finally:
+            try:
+                if conn:
+                    conn.close()
+            except Exception as e:
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
 
 class SignInSocialView(MethodView):
     def __init__(self, service):
         self.service = service
+        
+    @validate_params(
+        Param('id', JSON, str, rules=[Pattern("[a-z0-9]{4,100}$")], required=True),
+        Param('email', JSON, str, rules=[Pattern('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$')], required=True),
+        Param('account_type_id', JSON, str, required=True),
+        Param('user_type_id', JSON, str, required=True),
+        Param('social_type_id', JSON, str, required=True),
+        Param("social", JSON, str, required=True),
+        Param('receiving_event_is_agreed', JSON, bool, required=False),
+        Param('notifying_benefit_is_agreed', JSON, bool, required=False)
+    )
+    def post(self, valid: ValidRequest):
+        try:
+            body = valid.get_json()
+            conn = get_connection()
+            result = self.service.post_user_social_login(conn, body)
+            conn.commit()
+    
+            return post_response({
+                        "message" : "success", 
+                        "accessToken" : result['accessToken'],
+                        "account_type_id" : result['account_type_id'],
+                        "status_code" : 200
+                        }), 200
+        except Exception as e:
+            if conn:
+                conn.rollback()
+            raise e
+        finally:
+            try:
+                if conn:
+                    conn.close()
+            except Exception as e:
+                raise DatabaseCloseFail('서버에 알 수 없는 오류가 발생했습니다.')
+    
+        
+    
+    
     
